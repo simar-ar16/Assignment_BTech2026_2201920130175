0dc8b02b5cf0a05b9100ceaaab45cbff0b63f36f
Simarpreet Singh <simarpreet2916@gmail.com>
2025-01-26 23:51:38 +0530

    OOPS Problem

---
+++ 26.01.2025/Solution.java
@@ -0,0 +1,21 @@
+class Animal
+{
+    void walk()
+    {
+        System.out.println("I am walking");
+    }
+}
+class Bird extends Animal
+{
+void fly()
+{
+    System.out.println("I am flying");
+}
+}
+class Solution{
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+      bird.walk();
+      bird.fly();
+    }
+}
